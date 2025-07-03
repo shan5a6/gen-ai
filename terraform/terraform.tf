557c12173f9adb5bdc2ece8950e5624afcb0965c
shahan shaik <shahan.shaik@dnata.com>
2025-07-03 14:56:10 +0400

    update new changes

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,11 +1,20 @@
 terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "dvsmcb52025-bkt1"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = "~> 5.0"
     }
   }
 }
+
 provider "aws" {
-  region = var.region
+  region = "us-west-2"
 }
