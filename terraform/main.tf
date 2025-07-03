69b67d5becb0e9619ab989aa9aefd59f36f9b4bc
root <root@ip-172-31-38-217>
2025-07-03 09:09:12 +0000

    testing end to end

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-source = "./modules/vpc"
-vpc_cidr = var.vpc_cidr
-vpc_pubsubnet_cidr = var.vpc_pubsubnet_cidr
-vpc_privsubnet_cidr = var.vpc_privsubnet_cidr
+  source              = "./modules/vpc"
+  vpc_cidr            = var.vpc_cidr
+  vpc_pubsubnet_cidr  = var.vpc_pubsubnet_cidr
+  vpc_privsubnet_cidr = var.vpc_privsubnet_cidr
 }
